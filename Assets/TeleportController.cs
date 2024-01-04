e12908324147faec015a0345c4c5a8d208be21a0
Blake Lovelace <blake.lovelace@health.telstra.com>
2024-01-04 19:16:51 +0800

    Fix teleportation

--- Assets/TeleportController.cs
+++ Assets/TeleportController.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
-public class OnTeleport : MonoBehaviour
+public class TeleportController : MonoBehaviour
 {
     public InputActionReference teleportActivationReference;
-
     public UnityEvent onTeleportActivate;
     public UnityEvent onTeleportCancel;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
         teleportActivationReference.action.performed += TeleportModeActivate;
         teleportActivationReference.action.canceled += TeleportModeCancel;
     }
 
     private void TeleportModeActivate(InputAction.CallbackContext obj) => onTeleportActivate.Invoke();
-    private void TeleportModeCancel(InputAction.CallbackContext obj) => onTeleportCancel.Invoke();
+    private void TeleportModeCancel(InputAction.CallbackContext obj) => Invoke(nameof(DeactivateTeleporter), 0.1f);
+    private void DeactivateTeleporter() => onTeleportCancel.Invoke();
 }
